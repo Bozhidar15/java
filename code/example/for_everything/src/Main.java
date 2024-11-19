6e845786aeb3d22088c8456a23b1c45d28445615
Bozhidar15 <bojidaruse@gmail.com>
2024-11-19 10:41:35 +0200

    delete temporary files

--- code/example/for_everything/src/Main.java
+++
@@ -1,5 +0,0 @@
-import java.net.NetworkInterface;
-import java.util.Collections;
-
-public class Main {
-}
--- code/example/for_everything/src/bg/sofia/uni/fmi/mjt/vault/api/InformationForThePassword.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.vault.api;
-
-public record InformationForThePassword(boolean revealedInExposure, int relativeExposureFrequency, int exposureCount) {
-}
